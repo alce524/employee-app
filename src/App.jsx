5b47c365d73dbabc9c999e4e0273d2ef6277ea51
Shabbiby-GIT <shabbibymelo@gmail.com>
2026-03-28 16:12:27 -0400

    git commit -m correction

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,6 @@ import { useState } from "react";
 import EmployeeForm from "./components/EmployeeForm";
 
 function App() {
-  const [employees, setEmployees] = useState([]);
-
-  const addEmployee = (employee) => {
-    setEmployees((prev) => [...prev, employee]);
-  };
-
   return (
     <div>
       <EmployeeForm addEmployee={addEmployee} />
